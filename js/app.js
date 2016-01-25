dcf1cf776d741324d6dae462c58587aaf3fb8ea6
coolclouds <haris-sattar@hotmail.com>
2016-01-24 18:49:42 -0600

    Delete app.js

--- js/app.js
+++
@@ -1,6 +0,0 @@
-angular.module("app", ['ngMaterial'])
-.config(function($mdThemingProvider) {
-    $mdThemingProvider.theme('default')
-        .primaryPalette('light-blue',{'default': '50'})
-        .accentPalette('green',{'default': '700'});
-});
